1d310257c6f34a95a18f282da2cf456a0c13101f
OMOCHI2 <satsukisama.mochi@gmail.com>
2023-05-15 13:33:22 +0000

    modify module

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
+  def string_shuffle(s)
+    s.split("").shuffle.join
+  end
 end
